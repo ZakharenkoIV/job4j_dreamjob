edd09b37645f1dfed2d7d1e65515f792818efc2e
ZakharenkoIV <zaharenkoiv93@gmail.com>
2021-06-06 15:34:01 +0300

    1. Техническое задание - проект "Работа мечты" [#260362]

---
+++ src/main/java/ru/job4j/model/PersonnelOfficer.java
@@ -0,0 +1,22 @@
+package ru.job4j.model;
+
+public class PersonnelOfficer {
+    private String name;
+    private int Id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+}
