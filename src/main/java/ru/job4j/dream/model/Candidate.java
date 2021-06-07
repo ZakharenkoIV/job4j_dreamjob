51feec246bed6e5d74671bd95856d25f10d17406
ZakharenkoIV <zaharenkoiv93@gmail.com>
2021-06-07 12:23:22 +0300

    4. candicates.jsp - список кандидатов. [#276522]

--- src/main/java/ru/job4j/dream/model/Candidate.java
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -1,14 +1,13 @@
 package ru.job4j.dream.model;
 
+import java.util.Objects;
+
 public class Candidate {
-    private String name;
     private int id;
+    private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Candidate(int id, String name) {
+        this.id = id;
         this.name = name;
     }
 
@@ -18,5 +17,31 @@ public class Candidate {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id
+                && Objects.equals(name, candidate.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
