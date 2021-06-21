61876dee718b241b5bf0ef6105f0f354b2bc0b20
ZakharenkoIV <zaharenkoiv93@gmail.com>
2021-06-21 21:18:03 +0300

    0. MVC в Servlet. [#6868]

--- src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
@@ -3,12 +3,21 @@ package ru.job4j.dream.servlet;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.store.Store;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CandidateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("candidates", Store.instOf().findAllCandidates());
+        req.getRequestDispatcher("candidates.jsp").forward(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
@@ -18,6 +27,6 @@ public class CandidateServlet extends HttpServlet {
                         req.getParameter("name")
                 )
         );
-        resp.sendRedirect(req.getContextPath() + "/candidate/candidates.jsp");
+        resp.sendRedirect(req.getContextPath() + "/candidate/candidates.do");
     }
 }
